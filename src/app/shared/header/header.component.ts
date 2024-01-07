e4cc839896545c88ebe52ae6c0bcaa323952c5da
itsnabeelk <itsnabeelk@gmail.com>
2024-01-07 20:54:14 +0300

    add

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -17,30 +17,7 @@ export class HeaderComponent implements OnInit {
   }
   ngOnInit(): void {
     scriptMain();
-    const burgerBtn = document.querySelector('.burger-btn') as HTMLElement;
-    const menuWrapper = document.querySelector('.menu-wrapper') as HTMLElement;
-    const menuCloseBtn = document.querySelector('.menu-overlay__close') as HTMLElement;
-    const menuLinks = document.querySelectorAll('.menu-overlay__link') as NodeListOf<HTMLElement>;
-  
-    // Toggle the menu overlay
-    function toggleMenu() {
-      menuWrapper.style.display = 'block';
-      window.scrollTo(0, 0);
-    }
-  
-    // Close the menu overlay
-    function closeMenu() {
-      menuWrapper.style.display = 'none';
-    }
-  
-    // Attach event listeners
-    burgerBtn.addEventListener('click', toggleMenu);
-    menuCloseBtn.addEventListener('click', closeMenu);
-  
-    // Close the menu overlay when any link is clicked
-    menuLinks.forEach((link) => {
-      link.addEventListener('click', closeMenu);
-    });
+
   }
   
 }
